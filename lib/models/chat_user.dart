b4ae6f16bf14cbd4b6cbda53ff1989b132dc12fd
mgorem <oremgitonga22@gmail.com>
2022-06-09 18:29:22 +0300

    conceal App ID

--- lib/models/chat_user.dart
+++ lib/models/chat_user.dart
@@ -1,3 +1,8 @@
+// Consists of how user details will be required
+/*Consists of two functions with map and factory method
+  to read data from the snapshot that Firebase Firestore
+  returns
+*/ 
 class chatUser extends Equatable {
   final String id;
   final String photoUrl;
@@ -6,12 +11,9 @@ class chatUser extends Equatable {
   final String aboutMe;
 
   const chatUser(
-  {
-    required this.id,
-    required this.photoUrl,
-    required this.displayName,
-    required this.phoneNumber,
-    required this.aboutMe
-}
-);
+  {required this.id,
+   required this.photoUrl,
+   required this.displayName,
+   required this.phoneNumber,
+   required this.aboutMe});
 }
--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,3 +1,9 @@
+///
+/**
+ * AuthProvider class handles google sign-n and sign-out methods
+ * It also checks whether the user is logged in or not
+ */
+///
 class AuthProvider extends ChangeNotifier {
   final GoogleSignIn googleSignIn;
   final FirebaseAuth firebaseAuth;
@@ -29,6 +35,39 @@ class AuthProvider extends ChangeNotifier {
         } else {
           return false;
         }
-        }
+      }
+  }
+  Future<bool> handleGoogleSignIn() async {
+    _status = Status.authenticating;
+    notifyListeners();
+
+    GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+   if (googleUser != null) {
+     GoogleSignInAuthentication? googleAuth = await googleUser.authentication;
+     final AuthCredential credential = GoogleAuthProvider.credential(
+       accessToken: googleAuth.accessToken,
+       idToken: googleAuth.idToken,
+     );
+
+     User? firebaseUser =
+         (await firebaseAuth.signInWithCredential(credential)).user;
+
+     if (firebaseUser != null) {
+       final QuerySnapshot result = await firebaseFirestore
+           .collection(FirestoreConstants.pathUserCollection)
+           .where(FirestoreConstants.id, isEqualTo: firebaseUser.uid)
+           .get();
+       final List<DocumentSnapshot> document = result.docs;
+       if (document.isEmpty) {
+         firebaseFirestore
+             .collection(FirestoreConstants.pathUserCollection)
+             .doc(firebaseUser.uid)
+             .set({
+           FirestoreConstants.displayName: firebaseUser.displayName,
+           FirestoreConstants.photoUrl: firebaseUser.photoURL,
+           FirestoreConstants.id: firebaseUser.uid,
+           "createdAt: ": DateTime.now().millisecondsSinceEpoch.toString(),
+           FirestoreConstants.chattingWith: null
+         });}
   }
 }
